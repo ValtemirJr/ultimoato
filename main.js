c7d912ac985710cd87b731226e4704fae1a38577
Valtemir Junior <100030801+ValtemirJr@users.noreply.github.com>
2023-06-03 11:04:40 -0300

    add fog

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = zeroLeft(Math.floor(distance / (1000 * 60 * 60 * 24)));
+  var hours = zeroLeft(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  var minutes = zeroLeft(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+  var seconds = zeroLeft(Math.floor((distance % (1000 * 60)) / 1000));
 
   // Display the result in the element with id="demo"
   document.getElementById("timer").innerHTML = days + "d " + hours + "h "
@@ -26,3 +26,6 @@ var x = setInterval(function() {
     document.getElementById("timer").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+const zeroLeft = (n) =>
+  Math.floor(n).toString().padStart(2, '0');
